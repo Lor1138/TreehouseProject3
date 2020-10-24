088759fbe747c9ae0dc9a950627d2b5c1a92457c
Laura OBrien <54803384+Lor1138@users.noreply.github.com>
2020-10-23 20:51:54 -0400

    updated JS file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
 
+const otherJob = document.querySelector('#other-title');
+const shirtColor = document.querySelector('#color');
+const shirtTheme = document.querySelector('#design')
+const pickTheme = document.createElement("option");
 
-function nameFocus() {
-    document.getElementById("name").focus();
-}
 
-nameFocus(name);
+
+
+
+    //This function autofocuses the name input when the browser loads.
+    function nameFocus() {
+        document.getElementById("name").focus();
+    }
+        nameFocus(name);
+
+        //displays only if JavaScript is not enabled.
+        otherJob.style.display = 'none';
+
+        //disables color menu for tshirt until a theme is chosen
+        pickTheme.value = "pick-theme";
+        pickTheme.text = "Please select a T-shirt theme"
+        pickTheme.selected = true;
+        shirtColor.add(pickTheme);
+        shirtColor.setAttribute("disabled", false);
+
+        //if "js puns" or "I heart JS" is selected use change handler to make the "colors" menu update
+        shirtTheme.addEventListener('change', () =>{
+            if (shirtTheme.option.value === "js puns"){
+                
+
+            } if (shirtTheme.option.value === "heart js"){
+
+            } else {
+                shirtColor.setAttribute("disabled", false);
+            }
+
+        });
+
+
+
+
+
