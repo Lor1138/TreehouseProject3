775a5560af0dae0ae1a2a965bc624219219c115b
Laura OBrien <54803384+Lor1138@users.noreply.github.com>
2020-11-03 19:00:39 -0500

    Tshirt Selection working!!!

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-
-const otherJob = document.querySelector('#other-title');
-const shirtColor = document.querySelector('#color');
+const otherJob = document.querySelector("#other-title");
+const shirtColor = document.querySelector("#color");
 const pickTheme = document.createElement("option");
-const jsPuns = document.querySelector("#jsPuns");
-const heartJS = document.querySelector("#heartJS");
 const shirtTheme = document.querySelector("#design");
 const jsPunsCol = document.querySelector("#js-puns");
 const heartJSCol = document.querySelector("#heart-js");
+const cornflowerblue = document.querySelector('option[value="cornflowerblue"]');
+const tomato = document.querySelector('option[value="tomato"]');
 
 
 
@@ -22,26 +21,49 @@ const heartJSCol = document.querySelector("#heart-js");
         //disables color menu for tshirt until a theme is chosen
         pickTheme.value = "pick-theme";
         pickTheme.text = "Please select a T-shirt theme"
-        pickTheme.selected = true;
         shirtColor.add(pickTheme);
+        pickTheme.selected = true;
+        pickTheme.hidden = true;
         shirtColor.setAttribute("disabled", false);
+      
+      
 
-    function showSel(){
-        console.log(shirtTheme.value);
-          let current_value = shirtTheme.value;
+        //if "js puns" or "I heart JS" is selected use change handler to make the "colors" menu update
+        shirtTheme.addEventListener('change', (event) => {
+                let target = event.target.value;
+                     if (target === "heart js") {
+                        jsPunsCol.hidden = true;
+                        heartJSCol.hidden = false;
+                        tomato.selected = true;
+                        shirtColor.removeAttribute("disabled", true);
+                    } else if (target === "js puns") {
+                        heartJSCol.hidden = true;
+                        jsPunsCol.hidden = false;
+                        cornflowerblue.selected = true;
+                        shirtColor.removeAttribute("disabled", true);
+                    } else {
+                        pickTheme.value = "pick-theme";
+                        pickTheme.text = "Please select a T-shirt theme"
+                        shirtColor.add(pickTheme);
+                        pickTheme.selected = true;
+                        pickTheme.hidden = true;
+                        shirtColor.setAttribute("disabled", false);
+                    }
+
+         
+        });
+
+        
+    
+/*
+let current_value = shirtTheme.value;
             if (current_value === "js puns"){
                 heartJSCol.hidden = true;
                 jsPunsCol.hidden = false;
-                jsPunsCol.selected = true;
-                pickTheme.remove();
-                pickTheme.selected = false;
                 shirtColor.removeAttribute("disabled", true);
             } else if (current_value === 'heart js'){
                 jsPunsCol.hidden = true;
                 heartJSCol.hidden = false;
-                heartJSCol.selected = true;
-                pickTheme.remove();
-                pickTheme.selected = false;
                 shirtColor.removeAttribute("disabled", true);
             } else if(current_value === "Select Theme") {
                 shirtColor.setAttribute("disabled", false);
@@ -50,15 +72,4 @@ const heartJSCol = document.querySelector("#heart-js");
 
             }
         
-    }
-
-        //if "js puns" or "I heart JS" is selected use change handler to make the "colors" menu update
-        shirtTheme.addEventListener('change', () => {
-               showSel();
-
-        });
-
-        
-    
-
-
+*/
